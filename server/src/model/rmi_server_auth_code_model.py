bb87746674e58395ebef73c06a7bf6f7f605fa3b
misaruto <misaelg.freitas2000@gmail.com>
2024-07-07 16:15:23 -0300

    Refactor ajustando models para ter print dos objetos e corrigindo referencias das foreign keys

--- server/src/model/rmi_server_auth_code_model.py
+++ server/src/model/rmi_server_auth_code_model.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, BigInteger, String, DateTime, Boolean, ForeignKey, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.schema import PrimaryKeyConstraint, ForeignKeyConstraint
+from src.model.rmi_server_model import RmiServerModel
 
 Base = declarative_base()
 
@@ -9,7 +10,7 @@ class RmiServerAuthCodeModel(Base):
     __table_args__ = {'schema': 'museu'}
 
     id_rmi_server_auth_code = Column(BigInteger, primary_key=True, autoincrement=True)
-    id_rmi_server = Column(BigInteger, ForeignKey('museu.rmi_server.id_rmi_server'), nullable=False)
+    id_rmi_server = Column(BigInteger, nullable=False)
     cd_rmi_server_auth = Column(String(6), nullable=False)
     dt_created = Column(DateTime, nullable=False, default=func.getdate())
     in_accessed = Column(Boolean, nullable=False, default=False)
@@ -17,5 +18,12 @@ class RmiServerAuthCodeModel(Base):
 
     __table_args__ = (
         PrimaryKeyConstraint('id_rmi_server_auth_code', name='PK_rmi_server_auth_code'),
-        ForeignKeyConstraint(['id_rmi_server'], ['museu.rmi_server.id_rmi_server'], name='FK01_rmi_server_auth_code_X_rmi_server')
+        ForeignKeyConstraint(['id_rmi_server'], [RmiServerModel.id_rmi_server], name='FK01_rmi_server_auth_code_X_rmi_server')
     )
+    def __repr__(self):
+        return (
+            f"<RmiServerAuthCodeModel(id_rmi_server_auth_code={self.id_rmi_server_auth_code}, "
+            f"id_rmi_server={self.id_rmi_server}, cd_rmi_server_auth='{self.cd_rmi_server_auth}', "
+            f"dt_created='{self.dt_created}', in_accessed={self.in_accessed}, "
+            f"dt_accessed='{self.dt_accessed}')>"
+        )
--- server/src/model/rmi_server_session_model.py
+++ server/src/model/rmi_server_session_model.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, BigInteger, String, DateTime, Boolean, ForeignKey, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.schema import PrimaryKeyConstraint, ForeignKeyConstraint
-
+from src.model.rmi_server_auth_code_model import RmiServerAuthCodeModel
 Base = declarative_base()
 
 class RmiServerSessionModel(Base):
@@ -18,5 +18,15 @@ class RmiServerSessionModel(Base):
 
     __table_args__ = (
         PrimaryKeyConstraint('id_rmi_server_session', name='PK_rmi_server_session'),
-        ForeignKeyConstraint(['id_rmi_server_auth_code'], ['museu.rmi_server_auth_code.id_rmi_server_auth_code'], name='FK01_rmi_server_session_X_rmi_server_auth_code')
+        ForeignKeyConstraint(['id_rmi_server_auth_code'], [RmiServerAuthCodeModel.id_rmi_server_auth_code], name='FK01_rmi_server_session_X_rmi_server_auth_code')
     )
+    def __repr__(self):
+        return (
+            f"<RmiServerSessionModel(id_rmi_server_session={self.id_rmi_server_session}, "
+            f"id_rmi_server_auth_code={self.id_rmi_server_auth_code}, "
+            f"in_rmi_server_session_expired={self.in_rmi_server_session_expired}, "
+            f"dtf_expected_rmi_server_session='{self.dtf_expected_rmi_server_session}', "
+            f"dtf_rmi_server_session='{self.dtf_rmi_server_session}', "
+            f"dt_created='{self.dt_created}', "
+            f"nm_user_created_session='{self.nm_user_created_session}')>"
+        )
