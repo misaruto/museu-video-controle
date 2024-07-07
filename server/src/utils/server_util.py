de6fecb90cd93ad41f69bb70a4b5d18c5de9b568
misaruto <misaelg.freitas2000@gmail.com>
2024-07-07 16:19:04 -0300

    feat adicionando fluxo de criação e inserção de codigo de autenticação do servidor

--- server/src/utils/server_util.py
+++ server/src/utils/server_util.py
@@ -1,9 +1,18 @@
 from src.model.rmi_server_model import RmiServerModel
+from src.model.rmi_server_auth_code_model import RmiServerAuthCodeModel
 from src.repository.rmi_server_repository import RmiServerRepository
+from src.repository.rmi_server_auth_code_repository import RmiServerAuthCodeRepository
 
+from src.utils.randon_code_util import random_generator
 class StartServer:
-    def __init__(self,rmi_server_repository:RmiServerRepository,server_name:str,server_uri:str) -> None:
+    def __init__(self,
+                    rmi_server_repository:RmiServerRepository,
+                    rmi_server_auth_code_repository:RmiServerAuthCodeRepository,
+                    server_name:str,
+                    server_uri:str
+                ) -> None:
         self.rmi_server_repository = rmi_server_repository
+        self.rmi_server_auth_code_repository = rmi_server_auth_code_repository
         self.server_name = server_name
         self.server_uri = server_uri
 
@@ -14,5 +23,15 @@ class StartServer:
         )
         return self.rmi_server_repository.add_rmi_server(new_server)
 
+    def __register_server_auth_code(self,rmi_server:RmiServerModel):
+        randon_code = random_generator()
+        new_server_code = RmiServerAuthCodeModel(
+            id_rmi_server = rmi_server.id_rmi_server,
+            cd_rmi_server_auth = randon_code
+        )
+        print(new_server_code)
+        self.rmi_server_auth_code_repository.add_rmi_server_auth_code(self.rmi_server_repository,new_server_code)
     def start_server(self):
-        return self.__register_server()
+        rmi_server = self.__register_server()
+        print(rmi_server)
+        self.__register_server_auth_code(rmi_server)
