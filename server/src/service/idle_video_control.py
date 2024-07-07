65354c187325bfe8a572e20847a13c6f4c53dec7
Misael Guilhardes de Freitas <misaelg.freitas2000@gmail.com>
2024-07-07 12:50:21 -0300

    feat criando classes e interface para controle dos videos em idle

--- server/src/service/idle_video_control.py
+++ server/src/service/idle_video_control.py
@@ -1,11 +1,11 @@
 from abc import ABC, abstractmethod
 
 class IdleVideoControl(ABC):
-  @abstractmethod
-  def play():
-    raise NotImplementedError
-  
-  @abstractmethod
-  def stop():
-    raise NotImplementedError
+    @abstractmethod
+    def play():
+        raise NotImplementedError
+    
+    @abstractmethod
+    def stop():
+        raise NotImplementedError
 
