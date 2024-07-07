d33ac1f97ea6ee55483c793c1afba375c9abd7a0
misaruto <misaelg.freitas2000@gmail.com>
2024-07-07 18:42:11 -0300

    feat adicinando repositório de controle das sessões

---
+++ server/src/repository/impl/rmi_server_session_repository_impl.py
@@ -0,0 +1,37 @@
+from sqlalchemy.orm import Session
+from sqlalchemy import update,func
+from src.exceptions.not_found_exception import NotFoundException 
+from src.model.rmi_server_session_model import RmiServerSessionModel
+
+class RmiServerSessionRepositoryImpl():
+    def __init__(self,db_session:Session):
+        self.session = db_session
+    
+    def add_rmi_server_session(self, rmi_server_session:RmiServerSessionModel) -> RmiServerSessionModel:
+        self.session.add(rmi_server_session)
+        self.session.commit()
+        return rmi_server_session
+    
+    def increase_session_time(self,rmi_server_session:RmiServerSessionModel,amount_seconds:int):
+        stmt = update(RmiServerSessionModel)\
+            .where(RmiServerSessionModel.id_rmi_server_session == rmi_server_session.id_rmi_server_session)\
+            .where(RmiServerSessionModel.in_rmi_server_session_expired==False)\
+            .values(dtf_expected_rmi_server_session=func.dateadd('ss', RmiServerSessionModel.dtf_expected_rmi_server_session, amount_seconds))
+        self.session.execute(stmt)
+        self.session.commit()
+        return rmi_server_session
+    
+    def get_rmi_server_session(self,rmi_server_session:RmiServerSessionModel):
+        server_session = self.session.query(RmiServerSessionModel).filter(RmiServerSessionModel.id_rmi_server_session == rmi_server_session.id_rmi_server_session).first()
+        if not server_session:
+            raise NotFoundException("Sessão não encontrada")
+        return server_session
+
+    def end_session(self,rmi_server_session:RmiServerSessionModel):
+        stmt = update(RmiServerSessionModel)\
+            .where(RmiServerSessionModel.id_rmi_server_session == rmi_server_session.id_rmi_server_session)\
+            .where(RmiServerSessionModel.in_rmi_server_session_expired==False)\
+            .values(in_rmi_server_session_expired=True)
+        self.session.execute(stmt)
+        self.session.commit()
+        return rmi_server_session
---
+++ server/src/repository/rmi_server_session_repository.py
@@ -0,0 +1,13 @@
+from abc import ABC, abstractmethod
+from src.model.rmi_server_session_model import RmiServerSessionModel
+
+class RmiServerSessionRepository(ABC):
+    @abstractmethod
+    def add_rmi_server_session(self, rmi_server_session:RmiServerSessionModel) -> RmiServerSessionModel:
+        raise NotImplementedError
+    @abstractmethod
+    def increase_session_time(self,rmi_server_session:RmiServerSessionModel,amount_seconds:int):
+        raise NotImplementedError
+    @abstractmethod
+    def end_session(self,rmi_server_session:RmiServerSessionModel):
+        raise NotImplementedError
