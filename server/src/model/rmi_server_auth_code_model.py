8c9ba1c4e2a2ce08a77a6b535aaa96890422865a
misaruto <misaelg.freitas2000@gmail.com>
2024-07-07 13:42:10 -0300

    add data quando codigo de auth for acesso no model

--- server/src/model/rmi_server_auth_code_model.py
+++ server/src/model/rmi_server_auth_code_model.py
@@ -11,8 +11,9 @@ class RmiServerAuthCodeModel(Base):
     id_rmi_server_auth_code = Column(BigInteger, primary_key=True, autoincrement=True)
     id_rmi_server = Column(BigInteger, ForeignKey('museu.rmi_server.id_rmi_server'), nullable=False)
     cd_rmi_server_auth = Column(String(6), nullable=False)
-    in_accessed = Column(Boolean, nullable=False, default=False)
     dt_created = Column(DateTime, nullable=False, default=func.getdate())
+    in_accessed = Column(Boolean, nullable=False, default=False)
+    dt_accessed = Column(DateTime)
 
     __table_args__ = (
         PrimaryKeyConstraint('id_rmi_server_auth_code', name='PK_rmi_server_auth_code'),
