3e3146de1c2f8d9448f0a49dbea052652b5050d5
misaruto <misaelg.freitas2000@gmail.com>
2024-07-07 13:41:31 -0300

    add data em que codigo for acessado

--- database/sql/CREATE/0002_CREATE_rmi_server_auth_code.sql
+++ database/sql/CREATE/0002_CREATE_rmi_server_auth_code.sql
@@ -6,6 +6,7 @@ BEGIN
     cd_rmi_server_auth VARCHAR(6) NOT NULL,
     in_accessed BIT NOT NULL DEFAULT 0,
     dt_created DATETIME NOT NULL DEFAULT GETDATE(),
+    dt_accessed DATETIME NULL,
     CONSTRAINT PK_rmi_server_auth_code PRIMARY KEY (id_rmi_server_auth_code),
     CONSTRAINT FK01_rmi_server_auth_code_X_rmi_server FOREIGN KEY (id_rmi_server) REFERENCES rmi_server(id_rmi_server) 
   );
