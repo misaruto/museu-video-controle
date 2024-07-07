e64d50f41ae0132dc3669929d9bd0e9d7ea941b8
Misael Guilhardes de Freitas <misaelg.freitas2000@gmail.com>
2024-07-07 12:46:57 -0300

    add criando interface do repository da tabela de rmi_server_auth_code

--- server/src/repository/rmi_server_auth_code_repository.py
+++ server/src/repository/rmi_server_auth_code_repository.py
@@ -1,16 +1,16 @@
 from abc import ABC, abstractmethod
 from src.model.rmi_server_model import RmiServerModel
 
-class RmiServerRepository(ABC):
+class RmiServerAuthCodeRepository(ABC):
     @abstractmethod
-    def add_rmi_server(self, rmi_server:RmiServerModel):
+    def add_rmi_server_auth_code(self, rmi_server:RmiServerModel):
         raise NotImplementedError
     @abstractmethod
-    def get_rmi_server(self, id_rmi_server):
+    def get_rmi_server_auth_code(self, id_rmi_server_auth_code):
         raise NotImplementedError
     @abstractmethod
-    def get_all_servers(self):
+    def get_all_rmi_server_auth_code(self):
         raise NotImplementedError
     @abstractmethod
-    def deactivate_rmi_server(self, id_rmi_server):
+    def deactivate_rmi_server_auth_code(self, id_rmi_server_auth_code):
         raise NotImplementedError
