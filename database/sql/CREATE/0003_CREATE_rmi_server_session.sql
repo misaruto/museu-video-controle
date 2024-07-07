b5f99cbc1ddd720ad71ebf6cbf1b8a99ebd29ad9
misaruto <misaelg.freitas2000@gmail.com>
2024-07-07 19:33:24 -0300

    Add campo para registrar codigo e tempo da sessão

--- database/sql/CREATE/0003_CREATE_rmi_server_session.sql
+++ database/sql/CREATE/0003_CREATE_rmi_server_session.sql
@@ -3,8 +3,10 @@ BEGIN
   CREATE TABLE museu.dbo.rmi_server_session (
     id_rmi_server_session BIGINT NOT NULL IDENTITY(1,1),
     id_rmi_server_auth_code BIGINT NOT NULL,
+    cd_rmi_server_session_token VARCHAR(36) NOT NULL,
     in_rmi_server_session_expired BIT NOT NULL DEFAULT 0,
     dtf_expected_rmi_server_session DATETIME NOT NULL,
+    qt_session_duration_seconds INT NULL,
     dtf_rmi_server_session DATETIME NULL,
     dt_created DATETIME NOT NULL DEFAULT GETDATE(),
     nm_user_created_session VARCHAR(30) NULL,
