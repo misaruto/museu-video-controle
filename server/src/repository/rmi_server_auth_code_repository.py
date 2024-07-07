fddd9653a5ec1318f593e04060b1cafa4a5700b0
misaruto <misaelg.freitas2000@gmail.com>
2024-07-07 13:42:30 -0300

    fix interface do repositorio de auth code

--- server/src/repository/rmi_server_auth_code_repository.py
+++ server/src/repository/rmi_server_auth_code_repository.py
@@ -1,16 +1,10 @@
 from abc import ABC, abstractmethod
-from src.model.rmi_server_model import RmiServerModel
+from src.model.rmi_server_auth_code_model import RmiServerAuthCodeModel
 
 class RmiServerAuthCodeRepository(ABC):
     @abstractmethod
-    def add_rmi_server_auth_code(self, rmi_server:RmiServerModel):
+    def add_rmi_server_auth_code(self, rmi_server_auth_code:RmiServerAuthCodeModel):
         raise NotImplementedError
     @abstractmethod
-    def get_rmi_server_auth_code(self, id_rmi_server_auth_code):
-        raise NotImplementedError
-    @abstractmethod
-    def get_all_rmi_server_auth_code(self):
-        raise NotImplementedError
-    @abstractmethod
-    def deactivate_rmi_server_auth_code(self, id_rmi_server_auth_code):
+    def authenticate_user_with_rmi_server_code(self, cd_rmi_server_auth_code):
         raise NotImplementedError
