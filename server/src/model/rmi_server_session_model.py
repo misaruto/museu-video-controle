e5cd67589ce1000a6c6515bffc673e1cfa9c5f2b
misaruto <misaelg.freitas2000@gmail.com>
2024-07-07 23:02:55 -0300

    fix foreign key na model de sessão

--- server/src/model/rmi_server_session_model.py
+++ server/src/model/rmi_server_session_model.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, BigInteger, String, DateTime, Boolean, ForeignKey, func
+from sqlalchemy import Column, BigInteger,Integer, String, DateTime, Boolean, ForeignKey, func
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.schema import PrimaryKeyConstraint, ForeignKeyConstraint
 from src.model.rmi_server_auth_code_model import RmiServerAuthCodeModel
@@ -9,8 +9,10 @@ class RmiServerSessionModel(Base):
     __table_args__ = {'schema': 'museu'}
 
     id_rmi_server_session = Column(BigInteger, primary_key=True, autoincrement=True)
-    id_rmi_server_auth_code = Column(BigInteger, ForeignKey('museu.rmi_server_auth_code.id_rmi_server_auth_code'), nullable=False)
+    id_rmi_server_auth_code = Column(BigInteger, nullable=False)
     in_rmi_server_session_expired = Column(Boolean, nullable=False, default=False)
+    cd_rmi_server_session_token = Column(String(36),nullable=False)
+    qt_session_duration_seconds = Column(Integer),
     dtf_expected_rmi_server_session = Column(DateTime, nullable=False)
     dtf_rmi_server_session = Column(DateTime, nullable=True)
     dt_created = Column(DateTime, nullable=False, default=func.getdate())
